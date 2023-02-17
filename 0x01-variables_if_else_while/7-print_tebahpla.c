36f008ca6dbb406353ba6b0bc0d1b90b38ed4e4e
Keabetswe14 <londiwedlamini857@gmail.com>
2023-02-17 05:09:31 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in reverse
+ * Return: Always  0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar('\n');
+	return (0);
+}
