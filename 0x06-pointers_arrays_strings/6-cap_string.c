3a6e152478f659bd0f4740c8cd26907fbaaf4400
Keabetswe14 <londiwedlamini857@gmail.com>
2023-03-01 21:55:47 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
@@ -33,6 +32,5 @@ char *cap_string(char *str)
 
 		index++;
 	}
-
 	return (str);
 }
