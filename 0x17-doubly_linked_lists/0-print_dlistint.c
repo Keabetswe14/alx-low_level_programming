649f2f407eaf8fb5d197195b111765bd0d7aa821
Keabetswe14 <londiwedlamini857@gmail.com>
2023-06-16 13:30:17 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of a dlistint_t list
+ * @h: pointer to head of  DLL
+ * Return: the number of nodes type size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num_of_nodes;
+
+	num_of_nodes = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_of_nodes = num_of_nodes + 1;
+	}
+	return (num_of_nodes);
+}
