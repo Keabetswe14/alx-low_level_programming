e1f1f3e46fd2fbfede816fc2589f526efe1e98d2
Keabetswe14 <londiwedlamini857@gmail.com>
2023-07-10 11:43:20 -0700

    hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,11 +6,11 @@
 #include <string.h>
 
 /**
- * struct hash_node_s - Node of a hash table
+ * struct hash_node_s - Node of hash table
  * @key: The key, string
  * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * @value: The value corresponding to key
+ * @next: A pointer to next node of List
  */
 typedef struct hash_node_s
 {
@@ -21,7 +21,7 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
- * @size: The size of the array
+ * @size: The size of array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
